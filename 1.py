a4e5bd01403ed63e5d6ef4033bd3223bb8997a60
Y-Ellawah <200010357@must.edu.eg>
2026-01-31 22:55:10 +0200

    شرح بسيط للتعديل

--- 1.py
+++ 1.py
@@ -1,11 +1,6 @@
 import matplotlib
-import sklearn
-
 print("Matplotlib version:", matplotlib.__version__)
 print("Scikit-learn version:", sklearn.__version__)
-
 print("Everything OK ")
-
 print ("Hello world") #  Hellio world
 
-    
